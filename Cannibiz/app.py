6d5ca7dd4cb80d0c2408043c29b615031524d104
Joseph Flanagan <gamblinflanagan97@gmail.com>
2021-03-22 14:16:51 -0400

    update app.py

--- Cannibiz/app.py
+++ Cannibiz/app.py
@@ -27,34 +27,3 @@ if __name__ == '__main__':
         debug=True
     )
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-'''
-# Cannibiz-site
-## Created March 22nd 2021
-## This application will be the main webiste for Cannibiz
-
-### Languages used
-
-python
-html
-css
-javascript (react.js, node.js)
-
-### To Use:
-
-* Open a terminal shell and navigate to the desired directory 
-* Run `git clone https://github.com/Cannibiz/Cannibiz-site.git`
-
-
-### To run: 
-
-
-'''
